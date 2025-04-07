4a87ce2f2fb6c7fcfe2e7154dffa69d2b70210fd
kwasibaidoo <kwasi.baidoo@amalitech.com>
2025-04-07 11:23:28 +0000

    First Commit

---
+++ FileUploadFunction/src/test/java/fileuploadfunction/AppTest.java
@@ -0,0 +1,12 @@
+package fileuploadfunction;
+
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void successfulResponse() {
+    assertTrue(true);
+  }
+}
